1f498aa905dc3924c678dfecb2b55069dd2fd61f
Bob Sheehan <BobSheehanis@gmail.com>
2017-01-18 12:18:36 -0500

    general cleanup - bootstrap/comments
    use proper bootstrap classes, remove comments, refactor
    controller/webapi for maintainability

--- GeoWeb1/Controllers/GeoController.cs
+++ GeoWeb1/Controllers/GeoController.cs
@@ -9,58 +9,13 @@ using System.Web.Script.Services;
 using System.Web.Script.Serialization;
 using HtmlAgilityPack;
 using System.Text.RegularExpressions;
+using GeoWeb1.Scrapers;
+
 
 namespace WebApi.Controllers
 {
-    public class WhitePageHit
-    {
-        private string _name = null;
-        private string _address = null;
-        private string _phonenumber = null;
-  
-
-        public string phoneNumber { get { return _phonenumber; } set { _phonenumber = value; } }
-        public string address { get { return _address; } set { _address = value; } }
-        public string name { get { return _name; } set { _name = value; } }
-    }
-
-    public class Person {
-        private string _fname = null;
-        private string _lname = null;
-        private string _age = null;
-        public int seq = 0;
-        public string fName { get { return _fname; } set { _fname = value; } }
-        public string lName { get { return _lname; } set { _lname = value; } }
-        public string age { get { return _age; } set { _age = value; } }
-        public string phoneNumber { get; set; }
-    }
-
-
-    public class GenericResult {
-        public string result { get; set; }
-        public string payload { get; set; }
-    }
-
-
-    public class GenerticInfoHit
-    {
-        private List<Person> _MyPeopleInfo = new List<Person>();
-
-        public string result { get; set; }
-
-        public string sourceName { get; set; }
-        public string houseValue { get; set; }
-        public List<Person> peopleInfo { get { return _MyPeopleInfo; } set { _MyPeopleInfo = value; } }
-    }
-
-
-    public class PhoneLookupResult
-    {
-        public string result { get; set; }
-        public List<WhitePageHit> phoneList { get; set; }
-
-    }
-
+   
+    
     public class GeoController : ApiController
     {
 
@@ -85,7 +40,7 @@ namespace WebApi.Controllers
             string responseFromServer = "";
             try
             {
-                responseFromServer = GetURL(URL);
+                responseFromServer =  ScraperHelper.GetURL(URL);
 
             }
             catch (Exception ex)
@@ -110,7 +65,7 @@ namespace WebApi.Controllers
             try
             {
 
-                string responseFromServer = GetURL(URL);
+                string responseFromServer = ScraperHelper.GetURL(URL);
 
                 List<WhitePageHit> mynums = ParseWpList(responseFromServer);
 
@@ -139,7 +94,7 @@ namespace WebApi.Controllers
             try
             {
 
-                string responseFromServer = GetURL(URL);
+                string responseFromServer = ScraperHelper.GetURL(URL);
 
                 List<WhitePageHit> myPhoneNums = ParseWpList(responseFromServer);
                 List<WhitePageHit> toRemove = new List<WhitePageHit>();
@@ -171,21 +126,13 @@ namespace WebApi.Controllers
             string state = "", string zip = "")
         {
 
-            string URL = "http://www.spokeo.com/" + state + "/" + town + "/" + houseNumber + "-" + road.Replace(" ", "-");
-
+      
             try
             {
 
-                string responseFromServer = GetURL(URL);
-                
-                var html = new HtmlDocument();
-                html.LoadHtml(responseFromServer);
-                var root = html.DocumentNode;
-                // unused but a better way via linq... leaving here for now to refactor 
-                var residents = root.Descendants().Where(n => n.GetAttributeValue("id", "").Equals("property_resident_listview_section"));
-              
-                return Ok(ParseSpokeTo(html));
-
+                Spoketo myScraper = ScraperFactory.Create<Spoketo>(new CrankyAddress { state = state, city = town, road = road, housenum = houseNumber });
+                return Ok(myScraper.ScraperResults());
+            
             }
             catch (Exception ex)
             {
@@ -197,126 +144,8 @@ namespace WebApi.Controllers
 
 #region privateHelpers
 
-        private string GetURL(string URL)
-        {
-
-
-            try
-            {
-
-           
-                HttpWebRequest myReq =
-                (HttpWebRequest)WebRequest.Create(URL);
-
-             
-                    //begin fake useragent so external service won't deny us
-                myReq.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
-                myReq.UserAgent = "Mozilla / 5.0(Macintosh; Intel Mac OS X 10.10; rv: 50.0) Gecko / 20100101 Firefox / 50.0";
-                myReq.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
-
-                //end fake useragent so external service won't deny us
-
-                using  (WebResponse MyResponse = myReq.GetResponse())
-                {
-                    using (Stream dataStream = MyResponse.GetResponseStream())
-                    {
-                        // Open the stream using a StreamReader for easy access.
-                        StreamReader reader = new StreamReader(dataStream);
-                        // Read the content.
-                        return reader.ReadToEnd();
-                    }
-                }
-
-            }
-            catch (Exception ex)
-            {
-                return  ex.Message ;
-
-            }
-
-
-
-
-        }
-
-        private GenerticInfoHit ParseSpokeTo(HtmlDocument html)
-        {
-            GenerticInfoHit myResults = new GenerticInfoHit();
-            int hits = 0;
-            myResults.sourceName = "spoketo";
-
-
-            foreach (HtmlNode node in html.DocumentNode.SelectNodes("//div[@id='property_resident_listview_section']") ?? Enumerable.Empty<HtmlNode>())
-            {
-
-                string selector = "div[@class='listview_page']";
-
-
-                foreach (HtmlNode node2 in node.SelectNodes(selector) ?? Enumerable.Empty<HtmlNode>())
-                {
-
-                    foreach (HtmlNode listViewSection in node2.SelectNodes("div[@class='listview_section']") ?? Enumerable.Empty<HtmlNode>())
-                    {
-                        // GenerticInfoHit myGenericInfoHit = new GenerticInfoHit();
-                        foreach (HtmlNode listColumnSection in listViewSection.SelectNodes("div[@class='listview_section_column']") ?? Enumerable.Empty<HtmlNode>())
-                        {
-                            Person myPersonHit = new Person();  // assume one person in each .. pick up last one
-
-                            foreach (HtmlNode resSection in listColumnSection.SelectNodes("a[@class='listview_primary_title']") ?? Enumerable.Empty<HtmlNode>())
-                            {
-                                string Name = resSection.InnerText.Replace("*", "");
-                                var nameParts = Name.Split(' ');
-                                if (nameParts.Length > 0)
-                                {
-                                    myPersonHit.fName = nameParts[0];
-                                    myPersonHit.lName = nameParts[1];
-                                }
-
-                            }
-                            //look for age
-                            foreach (HtmlNode ageSection in listColumnSection.SelectNodes("div[@class='listview_primary_subtitle']") ?? Enumerable.Empty<HtmlNode>())
-                            {
-                                myPersonHit.age = ageSection.InnerText;
-                            }
-                            if (myPersonHit.lName != null)
-                            {
-                                myPersonHit.seq = hits;
-                                myResults.peopleInfo.Add(myPersonHit);
-                                hits++;
-                            }
-                        }
-
-                       
-                    }
-
-
-                }
-
-            }
-            myResults.result = hits.ToString();
-            myResults.houseValue = ParseSpokeToHomeValue(html);
-            return myResults;
-        }
-
-   
-         private string ParseSpokeToHomeValue(HtmlDocument html)
-        {
-            string HomeVal = "";
-            foreach (HtmlNode JewlNode in html.DocumentNode.SelectNodes("//div[@class='icon_jewel_blocks']") ?? Enumerable.Empty<HtmlNode>())
-            {
-                foreach (HtmlNode JewlBlock in JewlNode.SelectNodes("div[@class='icon_jewel_block']") ?? Enumerable.Empty<HtmlNode>())
-                {
-                    Regex Reggie = new Regex("\\<.*?>");
-                    string foobar = JewlBlock.InnerText;
-                    string bar = Reggie.Replace(foobar, " ");  // yup, hacky as all. Change to list of name value pairs
-                    if (bar.IndexOf("Home Value") > -1)
-                    {
-                        HomeVal = bar;
-                    }
-                }
-            }
-                return HomeVal;
-        }
+       
+ 
         private List<WhitePageHit> ParseWpList(string pageHtml)
         {
             var html = new HtmlDocument();
